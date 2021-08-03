2b0f432d788358d5cfba178b29334689badc5566
santiagodiazpace <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>
2021-08-03 15:35:02 -0300

    Soluciones posibles cultar texto de iconos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
+
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'pruebaMatIcons';
+
+
+  constructor() {
+
+    let delayInMilliseconds = 2000;
+    setTimeout(function() {
+      let elems = document.querySelectorAll<HTMLElement>(".myicon");
+      for(let i=0; i <= elems.length; i++) {
+        elems[i].style.opacity = "1";
+      }
+    }, delayInMilliseconds);
+
+  }
+  
+
 }
